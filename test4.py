a626cdb8adff0312fa7ec58a8f3edc875a517dbf
metdist <it@metdist.com>
2024-11-13 13:44:15 +0200

    chanmged the errors

--- test4.py
+++ test4.py
@@ -50,7 +50,7 @@ st.markdown(
 def color_status(val):
     color = (
         'background-color: green; color: white;'
-        if val.startswith("active") or val == "active, ready" or val == "online"
+        if val=="active" or val == "active, ready" or val == "online"
         else 'background-color: red; color: white;'
     )
     return color
